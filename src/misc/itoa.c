aa1a5013dcbd2ebac9590e65954cb33dba3d0313
Jaylon Gowie <jaylongowie46@gmail.com>
2024-03-13 00:53:20 -0500

    Fix itoa (For SDL2)

--- src/misc/itoa.c
+++ src/misc/itoa.c
@@ -1,10 +1,36 @@
-char *itoa(char *p, unsigned int x) {
-	// number of digits in a uint32_t + NUL
-	p += 11;
-	*--p = 0;
-	do {
-		*--p = '0' + x % 10;
-		x /= 10;
-	} while (x);
-	return p;
+int itoa(int value, char *sp, int radix)
+{
+    char tmp[16];
+    char *tp = tmp;
+    int i;
+    unsigned v;
+
+    int sign = (radix == 10 && value < 0);    
+    if (sign)
+        v = -value;
+    else
+        v = (unsigned)value;
+
+    while (v || tp == tmp)
+    {
+        i = v % radix;
+        v /= radix;
+        if (i < 10)
+          *tp++ = i+'0';
+        else
+          *tp++ = i + 'a' - 10;
+    }
+
+    int len = tp - tmp;
+
+    if (sign) 
+    {
+        *sp++ = '-';
+        len++;
+    }
+
+    while (tp > tmp)
+        *sp++ = *--tp;
+
+    return len;
 }

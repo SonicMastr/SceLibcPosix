9ccd3b24d25b1ba58d10392033c05aeaaec790d1
Jaylon Gowie <jaylongowie46@gmail.com>
2024-03-12 23:10:17 -0500

    Add early support for libstdc++

--- src/misc/newlibstub.c
+++ src/misc/newlibstub.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
-struct _reent *_impure_ptr = NULL;
-struct _reent *const _global_impure_ptr = NULL;
+static struct _reent impure_data = _REENT_INIT (impure_data);
+struct _reent *_impure_ptr = &impure_data;
+struct _reent *const _global_impure_ptr = &impure_data;
 
 void __sinit(struct _reent *s) {
 }

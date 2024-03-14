2005e7eb5492c2b115ceaa279d18af1bd3a58a9f
Jaylon Gowie <jaylongowie46@gmail.com>
2024-03-13 23:10:34 -0500

    Remove pointless __sinit

--- src/misc/newlibstub.c
+++ src/misc/newlibstub.c
@@ -3,6 +3,3 @@
 static struct _reent impure_data = _REENT_INIT (impure_data);
 struct _reent *_impure_ptr = &impure_data;
 struct _reent *const _global_impure_ptr = &impure_data;
-
-void __sinit(struct _reent *s) {
-}

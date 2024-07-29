d57ad90e32e40b13ccd93cd128cd9a6f1ef1782a
Jaylon Gowie <jaylongowie46@gmail.com>
2024-07-29 18:55:17 -0500

    Update Test.c

--- test/test.c
+++ test/test.c
@@ -10,14 +10,11 @@ int *_sceLibcErrnoLoc(void);
 
 #include <psp2/kernel/processmgr.h>
 
-extern int sceLibcGetFD(FILE *) __attribute__((weak));
-extern FILE *sceLibcGetFH(int) __attribute__((weak));
-
 extern const char sceUserMainThreadName[] = "test";
 extern const int sceUserMainThreadPriority = 0x10000100;
 extern const unsigned int sceUserMainThreadStackSize = 0x00040000U;
 
-unsigned int sceLibcHeapSize = 1 * 1024 * 1024;
+unsigned int sceLibcHeapSize = 32 * 1024 * 1024;
 
 void __hidden exit_func(void) {
 	printf("Exit_func\n");
@@ -27,7 +24,7 @@ int main(int argc, char const *argv[]) {
 	int ret;
 
 	int elf;
-	FILE *file = fopen("/sce_module/SceLibcExt.suprx", "rb");
+	FILE *file = fopen("/sce_module/SceLibcPosix.suprx", "rb");
 	fread(&elf, 4, 1, file);
 	printf("elf out: 0x%08X\n", elf);
 	fclose(file);
